a16b7fba581d4f6b426a2f894fd0c29be26d72ab
yezi <yejison98@gmail.com>
2022-06-23 22:15:27 +0900

    [Algo] 브루스포트

---
+++ Algorithm/Solution/손예지/[14501] 퇴사.py
@@ -0,0 +1,9 @@
+N = int(input(()))
+lst = [list(map(int, input().split(())) for _ in range(N))]
+dp = [0 for _ in range(N+1)]
+for i in range(N-1, -1, -1)  :
+    if i + lst[i][0] > N :
+        dp[i] = dp[i+1]
+    else :
+        dp[i] = max(dp[i+1], lst[i][1] + dp[i+lst[i][0]])
+print(dp[0])
