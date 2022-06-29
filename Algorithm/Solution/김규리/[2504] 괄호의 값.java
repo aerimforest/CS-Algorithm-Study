12b2197b8ed249034e587f7c03557fd120093453
Gyuri Kim <kgr3819@gmail.com>
2022-06-30 01:01:53 +0900

    [Algo] 구현 스택

--- Algorithm/Solution/김규리/[2504] 괄호의 값.java
+++ Algorithm/Solution/김규리/[2504] 괄호의 값.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Stack;
 
 public class Main {
@@ -15,52 +13,44 @@ public class Main {
         Stack<Character> stack = new Stack<>();
 
         for(int i=0; i<str.length(); i++){
-            switch (str.charAt(i)){
-                case '(':
-                    stack.push('(');
-                    num *= 2;
+            char ch = str.charAt(i);
+            if(ch == '('){
+                stack.push('(');
+                num *= 2;
+            } else if (ch == '['){
+                stack.push('[');
+                num *= 3;
+            } else if (ch == ')') {
+                //괄호가 잘못되어 있는 경우 처리
+                if (stack.isEmpty() || stack.peek() != '('){
+                    result = 0;
                     break;
-
-                case '[':
-                    stack.push('[');
-                    num *=3;
+                }
+                //괄호가 마무리 된 경우 결과에 반영
+                if (str.charAt(i-1) == '('){
+                    result += num;
+                }
+                //사용한 괄호는 스택에서 비움
+                stack.pop();
+                num /= 2;
+            } else if(ch == ']'){
+                //괄호가 잘못되어 있는 경우 처리
+                if (stack.isEmpty() || stack.peek() != '['){
+                    result = 0;
                     break;
-
-                case ')':
-                    //괄호가 잘못되어 있는 경우 처리
-                    if (stack.empty() || stack.peek() != '('){
-                        result = 0;
-                        break;
-                    }
-                    //괄호가 마무리 된 경우 결과에 반영
-                    if (str.charAt(i-1) == '('){
-                        result += num;
-                    }
-                    //사용한 괄호는 스택에서 비움
-                    stack.pop();
-                    num /= 2;
-                    break;
-
-                case ']':
-                    //괄호가 잘못되어 있는 경우 처리
-                    if (stack.empty() || stack.peek() != '['){
-                        result = 0;
-                        break;
-                    }
-                    //괄호가 마무리 된 경우 결과에 반영
-                    if (str.charAt(i-1) == '['){
-                        result += num;
-                    }
-                    //사용한 괄호는 스택에서 비움
-                    stack.pop();
-                    num /= 3; //다시 1로 초기화
-                    break;
+                }
+                //괄호가 마무리 된 경우 결과에 반영
+                if (str.charAt(i-1) == '['){
+                    result += num;
+                }
+                //사용한 괄호는 스택에서 비움
+                stack.pop();
+                num /= 3; //다시 1로 초기화
             }
         }
-        if(!stack.empty()){
-            System.out.println(0);
-        } else {
-            System.out.println(result);
+        if(!stack.isEmpty()){
+            result = 0;
         }
+        System.out.println(result);
     }
 }
